469a576a26368dc7bf8d9af88a8bcfb1bb896f6b
Felipe4job <felipe4job@gmail.com>
2024-02-03 18:14:38 -0300

    start project

---
+++ tailwind.config.ts
@@ -0,0 +1,55 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#f6e05e',
+        primaryLight: '#faf090',
+        primaryExLight: '#fefac5',
+        primaryDark: '#d69e2e',
+        primaryExDark: '#975a16',
+        secondary: '#90caf9',
+        secondaryLight: '#bbdefb',
+        secondaryExLight: '#e3f2fd',
+        secondaryDark: '#1e88e5',
+        secondaryExDark: '#1565c0',
+        green: '#22C55E',
+        greenLight: '#86EFAC',
+        greenExLight: '#BBF7D0',
+        greenDark: '#15803D',
+        greenExDark: '#14532D',
+        pink: '#EC4899',
+        pinkLight: '#F9A8D4',
+        pinkExLight: '#FBCFE8',
+        pinkDark: '#BE185D',
+        pinkExDark: '#831843',
+        red: '#DC2626',
+        redLight: '#F87171',
+        redExLight: '#FCA5A5',
+        redDark: '#B91C1C',
+        redExDark: '#7F1D1D',
+        gray: '#737373',
+        grayLight: '#D4D4D4',
+        grayExLight: '#F5F5F5',
+        grayDark: '#404040',
+        grayExDark: '#171717',
+      },
+      fontFamily: {
+        sans: [ 'Roboto', 'sans-serif' ],
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
